89f73755e33c4b1377cd486428fa62037c044fce
Austin Vanloon <wic404learn@gmail.com>
2025-09-21 23:21:57 -0600

    implement ride crud list, detail, create, update, delete

--- rides/urls.py
+++ rides/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import rides_index, rides_detail, RideCreate
+
+
 
 urlpatterns = [
-    path('', views.rides_index, name='index'),
-    path('<int:ride_id>/', views.rides_detail, name='detail'),
+    path('', rides_index, name='index'),
+    path('<int:ride_id>/', rides_detail, name='detail'),
+    path('create/', RideCreate.as_view(), name='rides_create'),
 ]
-
-
--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Ride
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView, DeleteView
+
 
 
 def rides_index(request):
@@ -7,7 +11,25 @@ def rides_index(request):
     return render(request, 'rides/index.html', { 'rides': rides })
 
 
-
 def rides_detail(request, ride_id):
     ride = Ride.objects.get(id=ride_id)
     return render(request, 'rides/detail.html', { 'ride': ride })
+
+
+class RideCreate(LoginRequiredMixin, CreateView):
+    model = Ride
+    fields = ['title', 'mileage', 'bike_make', 'bike_model', 'photo', 'attractions', 'notes', 'date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class RideUpdate(LoginRequiredMixin, UpdateView):
+    model = Ride
+    fields = ['title', 'mileage', 'bike_make', 'bike_model', 'photo', 'attractions', 'notes', 'date']
+
+class RideDelete(LoginRequiredMixin, DeleteView):
+    model = Ride
+    success_url = '/rides/'
+
+    
