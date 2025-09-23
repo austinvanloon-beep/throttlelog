e13a268cfff8c9a8441bb62e64548e2cad55192a
Austin Vanloon <wic404learn@gmail.com>
2025-09-22 23:25:03 -0600

    Add ownership checks and UI updates to rides index and detail templates

--- rides/urls.py
+++ rides/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from .views import rides_index, rides_detail, RideCreate, RideUpdate, RideDelete
-from django.contrib import admin
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rides/', include('rides.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.rides_index, name='index'),
+    path('<int:ride_id>/', views.rides_detail, name='detail'),
+    path('create/', views.RideCreate.as_view(), name='rides_create'),
+    path('<int:pk>/update/', views.RideUpdate.as_view(), name='update'),
+    path('<int:pk>/delete/', views.RideDelete.as_view(), name='delete'),
 ]
--- rides/views.py
+++ rides/views.py
@@ -5,10 +5,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView, DeleteView
 
 
-
+# rides private to user
 def rides_index(request):
     rides = Ride.objects.filter(user=request.user)
     return render(request, 'rides/index.html', { 'rides': rides })
+
+# rides public to all users
+# def rides_index(request):
+#    rides = Ride.objects.all()
+#    return render(request, 'rides/index.html', {'rides': rides})
 
 
 def rides_detail(request, ride_id):
@@ -28,7 +33,14 @@ class RideUpdate(LoginRequiredMixin, UpdateView):
     model = Ride
     fields = ['title', 'mileage', 'bike_make', 'bike_model', 'photo', 'attractions', 'notes', 'date']
 
+    def get_queryset(self):
+        return Ride.objects.filter(user=self.request.user)
+
+
 class RideDelete(LoginRequiredMixin, DeleteView):
     model = Ride
     success_url = '/rides/'
+
+    def get_queryset(self):
+        return Ride.objects.filter(user=self.request.user)
 
